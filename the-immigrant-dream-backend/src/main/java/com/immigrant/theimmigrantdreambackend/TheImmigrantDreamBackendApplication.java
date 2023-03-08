eaac4cba95ed017f1c72199f5cf49c2c606d0cf4
Jerome Joof <jeromejoof@Jeromes-MacBook-Pro.local>
2023-03-07 21:23:22 -0800

    Initial commit

---
+++ the-immigrant-dream-backend/src/main/java/com/immigrant/theimmigrantdreambackend/TheImmigrantDreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.immigrant.theimmigrantdreambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheImmigrantDreamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheImmigrantDreamBackendApplication.class, args);
+	}
+
+}
