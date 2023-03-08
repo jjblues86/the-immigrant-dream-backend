a75b9edd41d841724b36a7b3d7dfc102d0d5aba7
Jerome Joof <jeromejoof@Jeromes-MacBook-Pro.local>
2023-03-07 21:31:43 -0800

    Initial commit

---
+++ the-immigrant-dream-backend/src/test/java/com/immigrant/theimmigrantdreambackend/TheImmigrantDreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immigrant.theimmigrantdreambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheImmigrantDreamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
